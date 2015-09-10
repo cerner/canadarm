266520bf7ea3949b68d1d4bec7e8bc75f2829f45
Logan McCamon <logan@mccamon.org>
2015-09-09 21:52:05 -0500

    Add check for undefined logAttributes.msg in lib/handler/console.js

--- test/unit/handler/console.js
+++ test/unit/handler/console.js
@@ -6,15 +6,27 @@ var assert = require('assert'),
 
 describe('Canadarm', function (){
   describe('consoleLogHandler', function (){
+    var consoleLogSpy = sinon.spy(console, 'error');
+
+    afterEach(function() {
+      consoleLogSpy.reset();
+    });
+
     it('logs the attributes expected to the console with an = separator', function (){
-      var consoleLogSpy = sinon.spy(console, 'error'),
-        fakeLog = {one: 'one', two: 'two', msg: 'msg'},
-        logValues = '', key;
+      var fakeLog = {one: 'one', two: 'two', msg: 'msg'};
 
       Canadarm.Handler.consoleLogHandler(fakeLog);
 
       assert(consoleLogSpy.calledOnce);
       assert(consoleLogSpy.calledWith(fakeLog.msg, fakeLog));
+    });
+
+    it('does not log the attributes when the key "msg" is undefined', function() {
+      var fakeLog = {one: 'one', two: 'two'};
+
+      Canadarm.Handler.consoleLogHandler(fakeLog);
+
+      assert(consoleLogSpy.callCount === 0);
     });
   });
 });
